f11d7455ec52bb76e7cec742393f2f0c04faec99
Tracy <tracy.chacon.00@gmail.com>
2022-07-15 17:56:11 -0500

    worked through adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,14 +1,13 @@
 export const adjacentElementsProduct = (num: number[]): number => {
     let greater = num[0] * num[1];
 
-    for (let i = 0; i < num.length - 1; i++) {
+    for (let i = 1; i < num.length - 1; i++) {
         if ((num[i] * num[i + 1]) > greater) {
           greater = num[i] * num[i + 1];
         }
     }
     return greater;
 }
-
 
 let inputArr = [3, 6, -2, -5, 3];
 
