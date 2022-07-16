7e20bc9bb185bd0b0737e995ed01eacc6f915374
Tracy <tracy.chacon.00@gmail.com>
2022-07-15 22:34:37 -0500

    succinct version of adjacentElementProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,14 +1,27 @@
 export const adjacentElementsProduct = (num: number[]): number => {
-    let greater = num[0] * num[1];
+    let index = 0;
 
-    for (let i = 1; i < num.length - 1; i++) {
-        if ((num[i] * num[i + 1]) > greater) {
-          greater = num[i] * num[i + 1];
-        }
-    }
-    return greater;
+    const productsArr = num.map(element => element * num[++index] );
+    productsArr.pop();
+    return Math.max(...productsArr);
 }
 
-let inputArr = [3, 6, -2, -5, 3];
+console.log(adjacentElementsProduct([3, 6, -2, -5, 3, 7]));  
 
-console.log(adjacentElementsProduct(inputArr));  // prints 18
+
+// export const adjacentElementsProduct = (num: number[]): number => {
+//     let greater = num[0] * num[1];
+
+//     for (let i = 1; i < num.length - 1; i++) {
+//         if ((num[i] * num[i + 1]) > greater) {
+//           greater = num[i] * num[i + 1];
+//         }
+//     }
+//     return greater;
+// }
+
+// let inputArr = [3, 6, -2, -5, 3];
+
+// console.log(adjacentElementsProduct(inputArr));  // prints 18
+
+
