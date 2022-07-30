ec76f2ca09ee37c391b2dbe84175aac25cca2f32
Tracy <tracy.chacon.00@gmail.com>
2022-07-30 13:39:35 -0500

    working on it aplpahbeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,4 +1,24 @@
 export const alphabeticShift = (inputStr: string):string => {
+    const alphabet: string[] = ['a', 'b', 'c', 'e', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let charArr = inputStr.split('');
     
-    return "string";
+    for(let i = 0; charArr.length; i++) {
+        let index: number = 0;
+
+        if (charArr[i] !== 'z') {
+            index = alphabet.indexOf(alphabet[i]) + 1;
+        }
+       
+        charArr[i] = alphabet[index];
+        console.log(index);
+    }
+
+    // return charArr.join('');
+    return 'crazy';
 };
+
+console.log(alphabeticShift("crazy"));
+
+
+
+
