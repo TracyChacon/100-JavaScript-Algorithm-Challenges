556dc9c2f5619617571eb5bd7d22796ff13ddc46
Tracy <tracy.chacon.00@gmail.com>
2022-07-30 18:21:25 -0500

    alphabeticShift  problem worked

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,20 +1,18 @@
 export const alphabeticShift = (inputStr: string):string => {
-    const alphabet: string[] = ['a', 'b', 'c', 'e', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const alphabet: string[] = ['a', 'b', 'c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let charArr = inputStr.split('');
-    
-    for(let i = 0; charArr.length; i++) {
+      
+    for(let i = 0; i < charArr.length; i++) {
         let index: number = 0;
 
         if (charArr[i] !== 'z') {
-            index = alphabet.indexOf(alphabet[i]) + 1;
+            index = alphabet.indexOf(charArr[i]) + 1;
         }
        
-        charArr[i] = alphabet[index];
-        console.log(index);
+        charArr[i] = alphabet[index];  
     }
 
-    // return charArr.join('');
-    return 'crazy';
+    return charArr.join('');
 };
 
 console.log(alphabeticShift("crazy"));
